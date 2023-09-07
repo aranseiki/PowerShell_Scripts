d516ef3704964cbeae5deedda9553f96f4d14117
Allan de Oliveira Almeida <arumeidaaran@outlook.com>
2023-09-07 13:06:13 -0300

    Corrigido encodificação dos arquivos e limpado rastro do git nos mesmos

--- Componentes/ClickTimer.ps1
+++ Componentes/ClickTimer.ps1
@@ -7,6 +7,24 @@ $ScriptClickMouseButton = $DiretorioRaiz, 'Click-MouseButton.ps1' -join '\'
 
 Write-Host 'Iniciou às ' $(Get-Date -Format HH:mm:ss)
 
+
+<#
+    Opções possíveis de clique:
+
+    MOUSEEVENTF_MOVED,
+    MOUSEEVENTF_LEFTDOWN,
+    MOUSEEVENTF_LEFTUP,
+    MOUSEEVENTF_RIGHTDOWN,
+    MOUSEEVENTF_RIGHTUP,
+    MOUSEEVENTF_MIDDLEDOWN,
+    MOUSEEVENTF_MIDDLEUP,
+    MOUSEEVENTF_WHEEL,
+    MOUSEEVENTF_XDOWN,
+    MOUSEEVENTF_XUP,
+    MOUSEEVENTF_ABSOLUTE
+#>
+
+
 while ($true) {
     Click-MouseButton LEFT 10
     Write-Host 'Clicou às ' $(Get-Date -Format HH:mm:ss)
